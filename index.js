5510f0a1dac86b41e053b2ebdd8e7d3d3f7d1b1f
bidurkandel <kandelbidur11@gmail.com>
2021-05-06 12:33:49 -0500

    Made some changes

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000
 server.use(express.json())
 
 server.get('/api/*', (req, res) => {
-    res.json({
-        cohort: 'Web 41',
-    })
+  res.json({
+    cohort: 'Web 41',
+  })
 })
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+  console.log(`listening on ${PORT}`)
 })
